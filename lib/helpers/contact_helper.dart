f48889b1ee68945a5f90cbfc1132a910d6d8767f
Gileade Trindade <gileade.trindade@gmail.com>
2020-04-15 18:56:21 -0300

    Criando Close database

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -88,6 +88,10 @@ class ContactHelper {
     return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
+  Future close () async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
 
 }
 
