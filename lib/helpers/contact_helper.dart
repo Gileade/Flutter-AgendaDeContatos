ca2e73a4a07ced56f471e6ceff8ceeea67de5f1c
Gileade Trindade <gileade.trindade@gmail.com>
2020-04-21 15:58:07 -0300

    Add BackEnd

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -34,7 +34,7 @@ class ContactHelper {
     return await openDatabase(
         path, version: 1, onCreate: (Database db, int newerVersion) async {
       await db.execute(
-          "CREATE TABLE $contactTable ($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT"
+          "CREATE TABLE $contactTable ($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
               "$phoneColumn TEXT, $imgColumn TEXT)"
       );
     });
@@ -102,6 +102,8 @@ class Contact {
   String email;
   String phone;
   String img;
+
+  Contact();
 
   Contact.fromMap(Map map){
     id = map[idColumn];
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:agenda_de_contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
